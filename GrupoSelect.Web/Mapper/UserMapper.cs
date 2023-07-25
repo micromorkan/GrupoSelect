9383e35b8368dff4565f98aa5d9e58feebf7cfa0
Diego Andrade Sampaio <morkan@gmail.com>
2023-07-24 22:49:33 -0300

    configuração uppercase em todo o projeto

--- GrupoSelect.Web/Mapper/UserMapper.cs
+++ GrupoSelect.Web/Mapper/UserMapper.cs
@@ -9,13 +9,13 @@ namespace GrupoSelect.Web.Mapper
         {
             CreateMap<UserVM, User>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => $"{src.Id}")).ReverseMap().ForMember(dest => dest.Id, opt => opt.Ignore())
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.Name}")).ReverseMap().ForMember(dest => dest.Name, opt => opt.Ignore())
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.Name.ToUpperInvariant()}")).ReverseMap().ForMember(dest => dest.Name, opt => opt.Ignore())
                 .ForMember(dest => dest.Cnpj, opt => opt.MapFrom(src => $"{src.Cnpj}")).ReverseMap().ForMember(dest => dest.Cnpj, opt => opt.Ignore())
-                .ForMember(dest => dest.Representation, opt => opt.MapFrom(src => $"{src.Representation}")).ReverseMap().ForMember(dest => dest.Representation, opt => opt.Ignore())
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => $"{src.Email}")).ReverseMap().ForMember(dest => dest.Email, opt => opt.Ignore())
+                .ForMember(dest => dest.Representation, opt => opt.MapFrom(src => $"{src.Representation.ToUpperInvariant()}")).ReverseMap().ForMember(dest => dest.Representation, opt => opt.Ignore())
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => $"{src.Email.ToUpperInvariant()}")).ReverseMap().ForMember(dest => dest.Email, opt => opt.Ignore())
                 .ForMember(dest => dest.Login, opt => opt.MapFrom(src => $"{src.Login}")).ReverseMap().ForMember(dest => dest.Login, opt => opt.Ignore())
                 .ForMember(dest => dest.Password, opt => opt.MapFrom(src => $"{src.Password}")).ReverseMap().ForMember(dest => dest.Password, opt => opt.Ignore())
-                .ForMember(dest => dest.Profile, opt => opt.MapFrom(src => $"{src.Profile}")).ReverseMap().ForMember(dest => dest.Profile, opt => opt.Ignore())
+                .ForMember(dest => dest.Profile, opt => opt.MapFrom(src => $"{src.Profile.ToUpperInvariant()}")).ReverseMap().ForMember(dest => dest.Profile, opt => opt.Ignore())
                 .ForMember(dest => dest.Active, opt => opt.MapFrom(src => $"{src.Active}")).ReverseMap().ForMember(dest => dest.Active, opt => opt.Ignore());
         }
     }
